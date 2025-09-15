55659c8369744ad75d27cab47fc255402f7da67d
INFORM4TYK <kwoznicki10@gmail.com>
2025-09-15 18:26:08 +0200

    feat: add global loading function state

---
+++ frontend/src/context/LoadingContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useMemo } from "react";
+import { v4 as uuidv4 } from "uuid";
+import type { ReactNode } from "react";
+export interface LoadingContextType {
+  startLoading: () => () => void;
+  loading: string[];
+  startLoadingSilent: () => () => void;
+  loadingSilent: string[];
+}
+
+export const LoadingContext = createContext<LoadingContextType | null>(null);
+
+const LoadingProvider = ({ children }: { children: ReactNode }) => {
+  const [loading, setLoading] = useState<string[]>([]);
+  const [loadingSilent, setLoadingSilent] = useState<string[]>([]);
+
+  const startLoading = () => {
+    const id = uuidv4();
+    setLoading((prevLoading) => [...prevLoading, id]);
+
+    return () => {
+      setLoading((prevLoading) => prevLoading.filter((l) => l !== id));
+    };
+  };
+  const startLoadingSilent = () => {
+    const id = uuidv4();
+    setLoadingSilent((prevLoading) => [...prevLoading, id]);
+
+    return () => {
+      setLoadingSilent((prevLoading) => prevLoading.filter((l) => l !== id));
+    };
+  };
+  const value = useMemo(
+    () => ({
+      startLoading,
+      loading,
+      startLoadingSilent,
+      loadingSilent,
+    }),
+    [loading, loadingSilent]
+  );
+  return (
+    <LoadingContext.Provider value={value}>{children}</LoadingContext.Provider>
+  );
+};
+
+export default LoadingProvider;
