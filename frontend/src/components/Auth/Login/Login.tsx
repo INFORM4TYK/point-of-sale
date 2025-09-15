d1e79b04cbf3e08bb3e9de1e1a8183deabc18758
INFORM4TYK <kwoznicki10@gmail.com>
2025-09-15 17:59:00 +0200

    style: login page styles

--- frontend/src/components/Auth/Login/Login.tsx
+++ frontend/src/components/Auth/Login/Login.tsx
@@ -1,6 +1,76 @@
-
+import { Button, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+type LoginFormInputs = {
+  email: string;
+  password: string;
+};
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>();
+
+  const onSubmit = (data: LoginFormInputs) => {
+    console.log("Form data:", data);
+  };
+  return (
+    <section className="w-full h-screen grid place-items-center px-4">
+      <div className="h-1/2 w-full bg-primary absolute top-0 pattern-bg">
+        <div className="flex items-center flex-col justify-center h-full -mt-8">
+          <h1 className="z-50 text-4xl font-bold text-center text-textWhite mb-6">
+            Panel POS
+          </h1>
+        </div>
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="z-50 flex flex-col gap-4 bg-white w-full max-w-[500px] px-8 py-4 pt-6 rounded-xl shadow-lg border-2"
+      >
+        <h2 className="text-2xl font-bold mb-2  uppercase text-center text-textDark">
+          Zaloguj się
+        </h2>
+        <TextField
+          sx={{
+            marginBottom: !errors.password ? ".75rem" : "0",
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "10px",
+            },
+          }}
+          label="Email"
+          type="email"
+          {...register("email", { required: "Email jest wymagany" })}
+          error={!!errors.email}
+          helperText={errors.email?.message}
+        />
+        <TextField
+          sx={{
+            marginBottom: !errors.password ? ".75rem" : "0",
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "10px",
+            },
+          }}
+          label="Hasło"
+          type="password"
+          {...register("password", { required: "Hasło jest wymagane" })}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          className="btn--gradient btn--primary h-[50px]"
+          sx={{ borderRadius: "8px" }}
+        >
+          Zaloguj się
+        </Button>
+        <p className="text-center text-sm text-textDark mb-4">
+          Wprowadź dane, aby uzyskać dostęp do panelu sprzedaży
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2567E8",
+        danger: "#DC143C",
+        textDark: "#37353E",
+        textWhite: "#FEFEFE",
+        background: "#FEFEFE",
+      },
+    },
+  },
+  plugins: [],
+};
