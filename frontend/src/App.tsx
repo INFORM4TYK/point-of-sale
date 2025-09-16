88c8575b83c242d074dbfdf650029930524ce419
INFORM4TYK <kwoznicki10@gmail.com>
2025-09-16 15:19:20 +0200

    style: basic dashboard side menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path="/auth" element={<Authentication />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Navigate to="/dev" replace />} />
+        <Route path="/dev" element={<Dashboard />} />
+
+        {/* <Route path="/auth" element={<Authentication />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </>
   );
--- frontend/src/components/Auth/Dashboard/DashboardContent.tsx
+++ frontend/src/components/Auth/Dashboard/DashboardContent.tsx
@@ -1,33 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useLoading from "../../../hooks/useLoading";
 import useAuth from "../../../hooks/useAuth";
 import ProductList from "../../Products/ProductList";
+import DashboardSideMenu from "./Common/DashboardSideMenu";
 
 const DashboardContent = () => {
   const router = useNavigate();
   const { loading } = useLoading();
-  const { currentUser, loadingUser, logout } = useAuth();
+  // const { currentUser, loadingUser, logout } = useAuth();
+  const [activeTab, setActiveTab] = useState<
+    "dashboard" | "pos" | "products" | "orders" | "clients" | "settings"
+  >("dashboard");
 
-  useEffect(() => {
-    if (!loadingUser && !loading && !currentUser) {
-      router("/auth", { replace: true });
+  const currentUser = {
+    email: "example@gmail.com",
+  };
+  const renderTab = () => {
+    switch (activeTab) {
+      case "dashboard":
+        return <div>Witamy w dashboardzie</div>;
+      case "pos":
+        return <div>Witamy w POS</div>;
+      case "products":
+        return <div>Witamy w produkty</div>;
+      case "orders":
+        return <div>Witamy w zamówienia</div>;
+      case "clients":
+        return <div>Witamy w klienci</div>;
+      case "settings":
+        return <div>Witamy w ustawienia</div>;
+      default:
+        return <div>Witamy</div>;
     }
-  }, [loadingUser, loading, currentUser]);
-
-  if (loading && !currentUser) return;
-  if (!loading && !currentUser) {
-    return null;
-  }
+  };
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-2">Dashboard</h1>
-      <button onClick={() => logout()}>Wyloguj się</button>
-      <p>
-        Jesteś zalogowany jako: <strong>{currentUser?.email}</strong>
-      </p>
-      <div>To sa products</div>
-      {/* <ProductList /> */}
+    <div className="w-full h-screen flex">
+      <DashboardSideMenu activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      {/* Główny panel */}
+      <main className="flex-1 p-6 overflow-auto">
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-bold">{activeTab.toUpperCase()}</h1>
+          <button>Wyloguj się</button>
+        </div>
+
+        <p className="mb-6">
+          Jesteś zalogowany jako: <strong>{currentUser.email}</strong>
+        </p>
+
+        {renderTab()}
+      </main>
     </div>
   );
 };
